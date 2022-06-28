8de57d912c7e6d9455c7d2339a23e68df0b26d9d
SabbirHossain07 <sabbirsopnil07@gmail.com>
2022-06-28 23:53:15 +0600

    Initial Commit

---
+++ ContactsDemoIII/ContactsDemoIIIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsDemoIIIApp.swift
+//  ContactsDemoIII
+//
+//  Created by Sopnil Sohan on 28/6/22.
+//
+
+import SwiftUI
+
+@main
+struct ContactsDemoIIIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
